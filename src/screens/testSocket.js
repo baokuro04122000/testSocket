31020f2dcb2916413bd5f019c286801a7cad9e36
dinhbao <baotrandinh100@gmail.com>
2021-06-19 13:19:15 +0700

    test frontend side

--- src/screens/testSocket.js
+++ src/screens/testSocket.js
@@ -1,24 +1,24 @@
 import React, {useEffect} from 'react'
 import socketIOClient from 'socket.io-client';
-// const ENDPOINT = 
-// window.location.host.indexOf('localhost') >= 0
-// ? 'http://127.0.0.1:8000'
-// : window.location.host;
-// let sk;
+const ENDPOINT = 
+window.location.host.indexOf('localhost') >= 0
+? 'http://127.0.0.1:8000'
+: window.location.host;
+let sk;
 export default function TestSocket() {
-    // const getUser = () => {
-    //     sk.on('allUsers',({clients})=>{
-    //         console.log(clients);
-    //     })
-    // }
-    // useEffect(()=>{
-    //     sk = socketIOClient(ENDPOINT)
-    //     getUser()
-    // },[])
+    const getUser = () => {
+        sk.on('allUsers',({clients})=>{
+            console.log(clients);
+        })
+    }
+    useEffect(()=>{
+        sk = socketIOClient(ENDPOINT)
+        getUser()
+    },[])
        
     return (
         <>
-            {/* <button onClick={getUser}>get user</button> */}
+            <button onClick={getUser}>get user</button>
             <div>
                 hello  world
             </div>
