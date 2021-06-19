b3cf3cd961d4dc241685cdae09dc8b12dae5e93e
dinhbao <baotrandinh100@gmail.com>
2021-06-19 11:38:04 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import TestSocket from './screens/testSocket';
-import socketIOClient from 'socket.io-client';
-const ENDPOINT = window.location.host.indexOf('localhost') > 0
-                ? 'http://localhost:8000'
-                : window.location.host;
-let sk = socketIOClient(ENDPOINT); 
+ 
 function App() {
-  sk.on('allUsers',({clients})=>{
-    console.log(clients);
-  })  
   return (
     <Router>
       <Switch>
